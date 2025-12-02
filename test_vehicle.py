9a8ebbf63ac7fc2d582db909c1fe5b72e456b0f3
imjoe77 <nathanielbandi636@gmail.com>
2025-12-02 11:47:00 +0530

    Initial commit

---
+++ test_vehicle.py
@@ -0,0 +1,9 @@
+from vehicle import vehicle_details
+def test_vehicle_details():
+    expected_output=(
+    "vehicle_number = 101 \n",
+    "owner_name= Rohit \n",
+    "vehicle_type =Car \n",
+    "year= 2022"
+    )
+    assert vehicle_details("101","Rohit","Car","2020")==expected_output
---
+++ vehicle.py
@@ -0,0 +1,16 @@
+import pytest
+def vehicle_details(vehicle_number,owner_name,vehicle_type,year):
+    result = (
+        f"Vehicle Number:{vehicle_number} \n"
+        f"Owner Name: {owner_name}\n"
+        f"Vehicle Type: {vehicle_type}\n"
+        f"Year of manufacture: {year}"
+        )
+    return result
+if __name__ == "__main__":
+    #Sample Output
+    vehicle_number = "101"
+    owner_name= "Rohit"
+    vehicle_type ="Car"
+    year= 2022
+    print(vehicle_details(vehicle_number,owner_name,vehicle_type,year))
