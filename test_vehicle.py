d8f7edcbd42172b05ca819aefcc235ac271771ad
Joe <nathanielbandi636@gmail.com>
2025-12-02 11:55:31 +0530

    Update test_vehicle.py

--- test_vehicle.py
+++ test_vehicle.py
@@ -1,9 +1,11 @@
 from vehicle import vehicle_details
+
 def test_vehicle_details():
-    expected_output=(
-    "vehicle_number = 101 \n",
-    "owner_name= Rohit \n",
-    "vehicle_type =Car \n",
-    "year= 2022"
+    expected_output = (
+        "vehicle_number = 101 \n",
+        "owner_name = Rohit \n",
+        "vehicle_type = Car \n",
+        "year = 2020"
     )
-    assert vehicle_details("101","Rohit","Car","2020")==expected_output
+    
+    assert vehicle_details("101", "Rohit", "Car", "2020") == expected_output
